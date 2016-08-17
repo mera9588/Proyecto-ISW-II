0ae8815255973d05b2294ddd2f3cef127ddb5237
mera9588 <morodriguezar@est.utn.ac.cr>
2016-08-17 00:01:24 -0600

    Actualización de vista de inicio de sesión
    Se alineo al centro de la pantalla.

--- core/modules/ventas/view/login/widget-default.php
+++ core/modules/ventas/view/login/widget-default.php
@@ -1,37 +1,36 @@
 <?php
-
 if(Session::getUID()!=""){
-		print "<script>window.location='index.php?view=home';</script>";
+	print "<script>window.location='index.php?view=home';</script>";
 }
 ?>
 
 <br><br><br><br><br><br><br><br>
 <div class="row vertical-offset-100">
-    	<div class="col-md-4 col-md-offset-3">
-    	<?php if(isset($_COOKIE['password_updated'])):?>
-    		<div class="alert alert-success">
-    		<p><i class='glyphicon glyphicon-off'></i> Se ha cambiado la contraseña exitosamente!</p>
-    		<p>Pruebe iniciar sesión con su nueva contraseña.</p>
-    		</div>
-    	<?php setcookie("password_updated","",time()-18600);
-    	 endif; ?>
-    		<div class="panel panel-primary">
-			  	<div class="panel-heading">
-			    	<h3 class="panel-title">Inicio de sesión</h3>
-			 	</div>
-			  	<div class="panel-body">
-			    	<form accept-charset="UTF-8" role="form" method="post" action="index.php?view=processlogin">
-                    <fieldset>
-			    	  	<div class="form-group">
-			    		    <input class="form-control" placeholder="Usuario" name="mail" type="text">
-			    		</div>
-			    		<div class="form-group">
-			    			<input class="form-control" placeholder="Contraseña" name="password" type="password" value="">
-			    		</div>
-			    		<input class="btn btn-lg btn-primary btn-block" type="submit" value="Iniciar sesión">
-			    	</fieldset>
-			      	</form>
-			    </div>
+	<div class="col-md-4 col-md-offset-3">
+		<?php if(isset($_COOKIE['password_updated'])):?>
+		<div class="alert alert-success">
+			<p><i class='glyphicon glyphicon-off'></i> Se ha cambiado la contraseña exitosamente!</p>
+			<p>Pruebe iniciar sesión con su nueva contraseña.</p>
+		</div>
+		<?php setcookie("password_updated","",time()-18600);
+		endif; ?>
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				<h3 class="panel-title">Inicio de sesión</h3>
+			</div>
+			<div class="panel-body">
+				<form accept-charset="UTF-8" role="form" method="post" action="index.php?view=processlogin">
+					<fieldset>
+						<div class="form-group">
+							<input class="form-control" placeholder="Usuario" name="mail" type="text">
+						</div>
+						<div class="form-group">
+							<input class="form-control" placeholder="Contraseña" name="password" type="password" value="">
+						</div>
+						<input class="btn btn-lg btn-primary btn-block" type="submit" value="Iniciar sesión">
+					</fieldset>
+				</form>
 			</div>
 		</div>
+	</div>
 </div>
